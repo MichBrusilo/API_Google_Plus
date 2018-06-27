a2a3a2e9b93db1ddb4ca80f740ab655aa63c3095
Michał Brusiło <mibru@interia.pl>
2018-06-27 12:41:35 +0200

    Add config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '998080700916-ldu1r1a0eber3ui1u81nmapao72p8q57.apps.googleusercontent.com ',
+	'GOOGLE_CLIENT_SECRET': 'p7mMw5PJpt9m2-eebKn3lHqs',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
